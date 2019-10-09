82f57c456c14a133d363358d577bbbdcbfe6f21d
berryd1 <berryd1@tcd.ie>
2019-10-09 15:13:19 +0100

    Updated tests - now have two tests - node constructor and empty tree tests

--- LCA assignment/src/LCATests.java
+++ LCA assignment/src/LCATests.java
@@ -7,8 +7,14 @@ public class LCATests {
 	@Test 
 	public void treeEmptyTest(){
 		LCA emptyTree = new LCA();
-		assertEquals("LCA is empty: ",-1,emptyTree.findLowestCommonAncestor(0, 0));
+		assertEquals("Find LCA when tree is empty: ",-1,emptyTree.findLowestCommonAncestor(0, 0));
 	}
 	
+	@Test
+	public void nodeConstructorTest(){
+		
+		Node nodeConstruct = new Node(1);
+		assertNotNull("Node constructor being tested: ", nodeConstruct);
+	}
 	
 }
