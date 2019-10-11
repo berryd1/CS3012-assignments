6cda1077ef7898b6c85fec83a2b7c1c55c00ae21
berryd1 <berryd1@tcd.ie>
2019-10-11 11:00:19 +0100

    Common ancestor test added

--- LCA assignment/src/LCATests.java
+++ LCA assignment/src/LCATests.java
@@ -17,4 +17,25 @@ public class LCATests {
 		assertNotNull("Node constructor being tested: ", nodeConstruct);
 	}
 	
+	@Test
+	public void twoNodesLowestCommonAncestorTest()
+	{
+		LCA ancestorTest = new LCA();
+		ancestorTest.root = new Node(1);
+		ancestorTest.root.left = new Node(2);
+		ancestorTest.root.right = new Node(3);
+		ancestorTest.root.left.left = new Node(4);
+		ancestorTest.root.left.right = new Node(5);
+		ancestorTest.root.right.left = new Node(6);
+        ancestorTest.root.right.right = new Node(7);
+        
+        assertEquals("LCA of 4 and 5: ",2,ancestorTest.findLowestCommonAncestor(4, 5));
+        assertEquals("LCA of 6 and 7: ",3,ancestorTest.findLowestCommonAncestor(6, 7));
+        assertEquals("LCA of 2 and 5: ",2,ancestorTest.findLowestCommonAncestor(2, 5));
+        assertEquals("LCA of 1 and 7: ",1,ancestorTest.findLowestCommonAncestor(1, 7));
+        assertEquals("LCA of 1 and 6: ",1,ancestorTest.findLowestCommonAncestor(1, 6));
+        
+	}
+
+	
 }
