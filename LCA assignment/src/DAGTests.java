332af6abca687770fae38fc389efa373d45cc722
berryd1 <berryd1@tcd.ie>
2019-10-15 13:37:38 +0100

    Added three tests to DAGTests

--- LCA assignment/src/DAGTests.java
+++ LCA assignment/src/DAGTests.java
@@ -5,8 +5,31 @@ import org.junit.Test;
 public class DAGTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void directedGraphTest()
+	{
+		DAG test1 = new DAG(10);
+		test1.addEdge(1, 2);
+		test1.addEdge(1, 3);
+		test1.addEdge(3, 4);
+		test1.addEdge(4, 5);
+		test1.addEdge(4, 6);
+
+		assertEquals("", 5, test1.E());
+		assertEquals("", 10, test1.V());
+		String ans = "[5, 6]";
+		assertEquals("",ans, test1.adj(4).toString());
+	}
+	
+	@Test
+	public void inDegreeTest(){
+		DAG test2 = new DAG(5);
+		assertEquals("", -1, test2.indegree(-3));
+	}
+
+	@Test
+	public void outDegreeTest(){
+		DAG test3 = new DAG(5);
+		assertEquals("", -1, test3.outdegree(8));	
 	}
 
 }
