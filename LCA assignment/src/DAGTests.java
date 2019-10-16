6072f695297f523d8ae3d7ae30ed93c03f514432
berryd1 <berryd1@tcd.ie>
2019-10-16 16:27:17 +0100

    Completed DAG tests

--- LCA assignment/src/DAGTests.java
+++ LCA assignment/src/DAGTests.java
@@ -101,5 +101,60 @@ public class DAGTests {
 		assertEquals("Finding LCA when one node doesnt exist", -1, test7.findLowestCommonAncestor(7, 3));
 	}
 
+	@Test
+	public void LCAForNonDAGTest()
+	{
+		DAG test8 = new DAG(11);
+		//---0
+		//--|-\
+		//--2--3
+		
+		//0 - 2 - 3 make a cycle
+
+		test8.addEdge(0, 1);
+		test8.addEdge(0, 2);
+		test8.addEdge(2, 3);
+		test8.addEdge(3, 0);
+		test8.addEdge(3, 4);
+
+		//Returns -1 if graph is not a DAG
+		assertEquals("", -1, test8.findLowestCommonAncestor(2, 3));
+		assertEquals("", -1, test8.findLowestCommonAncestor(3, 4));
+		assertEquals("", -1, test8.findLowestCommonAncestor(1, 2));
+		assertEquals("", -1, test8.findLowestCommonAncestor(0, 3));
+		assertEquals("", -1, test8.findLowestCommonAncestor(1, 3));
+
+	}
+	
+	@Test
+	public void LCATest(){
+		DAG test9 = new DAG(10);
+
+		//--------2---5----7--
+		//---0--1-------6----8
+		//--------3---4-------
+		test9.addEdge(0, 1);
+		test9.addEdge(1, 2);
+		test9.addEdge(1, 3);
+		test9.addEdge(2, 5);
+		test9.addEdge(3, 4);
+		test9.addEdge(4, 6);
+		test9.addEdge(5, 6);
+		test9.addEdge(6, 8);
+		test9.addEdge(5, 7);
+		test9.addEdge(7, 8);
+		test9.addEdge(8, 9);
+
+		assertEquals("Finding LCA for 4 and 5", 1, test9.findLowestCommonAncestor(5, 4));
+		assertEquals("Finding LCA for 7 and 8", 7, test9.findLowestCommonAncestor(8, 7));
+		assertEquals("Finding LCA for 6 and 8", 6, test9.findLowestCommonAncestor(6, 8));
+		assertEquals("Special case where both parameters are same vertex", 2, test9.findLowestCommonAncestor(2,2));
+	}
+	
+	@Test(expected=Exception.class)
+	public void exceptionTest(){
+		//Cannot make a directed graph with less than 0 vertices
+		DAG test10 = new DAG(-5);
+	}
 
 }
